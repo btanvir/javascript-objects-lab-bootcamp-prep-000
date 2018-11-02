5e118428ca2a1d4cc1b9618584b1f535735659bf
Brighton Tanvir <brightontanvir@gmail.com>
2018-11-02 00:47:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function deleteFromObjectByKey(object, key) {
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
   var newObj=Object.assign({},object)
-   delete newObj.key
+   newObj[key] = undefined
    return newObj
 }
